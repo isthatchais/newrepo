b881446cc4caff80707f0c2dbf55756e60867799
Chais Chang <cha96001@byui.edu>
2023-10-11 18:52:56 -0600

    added try catch block to inventory-model.js  7-12

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,8 +4,13 @@ const pool = require("../database/")
  *  Get all classification data
  * ************************** */
 async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+  try{
+    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+  }catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
 }
+  
 
 module.exports = {getClassifications}
 
